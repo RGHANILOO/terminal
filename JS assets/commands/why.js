0338c40b43688ad06f4927089a868f75a2e049b1
“rezan5910” <bytemefirst@gmail.com>
2024-02-07 23:17:16 +0000

    feat: content for why section of the page fix: styling of the content paragraphs

--- JS assets/commands/why.js
+++ JS assets/commands/why.js
@@ -1,6 +1,6 @@
 export default function (output) {
    console.log(`why not is what i say? einstein said coz he can so can i`)
    output.innerHTML += ` 
-        I AM WHO I AM ,GET ME?
+        <p>I am applying to Founders and Coders (FAC) because it offers a dynamic and inclusive community that fosters both personal growth and collaboration. FAC's emphasis on community-building and social impact aligns perfectly with my values ambition to utilise technology for positive changes in our society. I am particularly drawn to FAC's full stack software development path as it provides an exciting opportunity for continuous learning and development while working on projects that make a meaningful difference. I believe that by immersing myself in FAC's wonderful community, I can not only enhance my technical skills but also contribute to impactful initiatives that line up with my values</p>
         `;
   }
