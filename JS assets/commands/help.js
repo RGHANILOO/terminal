86c7d6f30b052cf538a78cff2f038e6c83a1eb70
“rezan5910” <bytemefirst@gmail.com>
2024-02-12 10:51:19 +0000

    fix:indentationa dn verbiage

--- JS assets/commands/help.js
+++ JS assets/commands/help.js
@@ -6,16 +6,16 @@ export default function help (output) {
     <span data-color="grey">Key Application Commands: </span>
     <span data-color="grey">-------------------------</span>
 
-    <span data-color="lime">Who</span>\n  <span data-color="grey">-</span> <span data-color="yellow">Information about who i am.</span>
-    <span data-color="lime">Why</span>\n  <span data-color="grey">-</span> <span data-color="yellow">Why Founders and Coders and why career in software development </span>
-    <span data-color="lime">What</span>\n  <span data-color="grey">-</span> <span data-color="yellow">What i have buit and learned along the way.</span>
-    <span data-color="lime">Contact: </span>\n  <span data-color="grey">-</span> <span data-color="yellow">Here is how you can reach me</span>
+    <span data-color="lime">Who</span>\n  <span data-color="grey">-</span> <span data-color="yellow">Information about who i am</span>
+    <span data-color="lime">Why</span>\n  <span data-color="grey">-</span> <span data-color="yellow">Why Founders and Coders and why career in software development</span>
+    <span data-color="lime">What</span>\n  <span data-color="grey">-</span> <span data-color="yellow">What i have buit and learned along the way</span>
+    <span data-color="lime">Contact</span>\n  <span data-color="grey">-</span> <span data-color="yellow">Here is how you can reach me</span>
 
     <span data-color="grey">Additional Commands: </span>
     <span data-color="grey">--------------------</span>
 
-    <span data-color="lime">Codewars:</span>\n  <span data-color="grey">-</span> <span data-color="yellow">A list of all the completed かた having reached きゅうご</span>
-    <span data-color="lime">CV:</span>\n  <span data-color="grey">-</span> <span data-color="yellow">What i have buit and learned along the way.</span>
+    <span data-color="lime">Codewars</span>\n  <span data-color="grey">-</span> <span data-color="yellow">A list of all the completed かた having reached きゅうご</span>
+    <span data-color="lime">CV</span>\n  <span data-color="grey">-</span> <span data-color="yellow">What i have buit and learned along the way</span>
     <span data-color="lime">Clear</span>\n  <span data-color="grey">-</span> <span data-color="yellow">Clear the terminal</span>
     
     `;
