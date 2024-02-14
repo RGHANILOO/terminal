d5451ad52af3129c47763f656549386c28bcc1eb
“rezan5910” <bytemefirst@gmail.com>
2024-02-14 14:20:36 +0000

    fix:color scheme issue, dark and light mode working

--- JS assets/commands/help.js
+++ JS assets/commands/help.js
@@ -6,17 +6,17 @@ export default function help (output) {
     <span data-color="grey">Key Application Commands: </span>
     <span data-color="grey">-------------------------</span>
 
-    <span data-color="lime">who</span>\n  <span data-color="grey">-</span> <span data-color="yellow">Information about who i am</span>
-    <span data-color="lime">why</span>\n  <span data-color="grey">-</span> <span data-color="yellow">Why Founders and Coders and why career in software development</span>
-    <span data-color="lime">what</span>\n  <span data-color="grey">-</span> <span data-color="yellow">What i have buit and learned along the way</span>
-    <span data-color="lime">contact</span>\n  <span data-color="grey">-</span> <span data-color="yellow">Here is how you can reach me</span>
+    <span data-color="#2bad5d">who</span>\n  <span data-color="grey">-</span> <span data-color="#d06b33">Information about who i am</span>
+    <span data-color="#2bad5d">why</span>\n  <span data-color="grey">-</span> <span data-color="#d06b33">Why Founders and Coders and why career in software development</span>
+    <span data-color="#2bad5d">what</span>\n  <span data-color="grey">-</span> <span data-color="#d06b33">What i have built and learned along the way</span>
+    <span data-color="#2bad5d">contact</span>\n  <span data-color="grey">-</span> <span data-color="#d06b33">Here is how you can reach me</span>
 
     <span data-color="grey">Additional Commands: </span>
     <span data-color="grey">--------------------</span>
 
-    <span data-color="lime">codewars</span>\n  <span data-color="grey">-</span> <span data-color="yellow">A list of all the completed かた having reached きゅうご</span>
-    <span data-color="lime">cv</span>\n  <span data-color="grey">-</span> <span data-color="yellow">You can download a copy of my CV here</span>
-    <span data-color="lime">clear</span>\n  <span data-color="grey">-</span> <span data-color="yellow">Clear the terminal</span>
+    <span data-color="#2bad5d">codewars</span>\n  <span data-color="grey">-</span> <span data-color="#d06b33">A list of all the completed かた having reached きゅうご</span>
+    <span data-color="#2bad5d">cv</span>\n  <span data-color="grey">-</span> <span data-color="#d06b33">You can download a copy of my CV here</span>
+    <span data-color="#2bad5d">clear</span>\n  <span data-color="grey">-</span> <span data-color="#d06b33">Clear the terminal</span>
     
     `;
 }
